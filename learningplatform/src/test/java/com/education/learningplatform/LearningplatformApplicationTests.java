0489a920eb642081ddc3a72c6288afaea620a2da
rafaella-vlase <kmyelly2@gmail.com>
2024-12-01 16:06:59 +0200

    initial commit

---
+++ learningplatform/src/test/java/com/education/learningplatform/LearningplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.education.learningplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
